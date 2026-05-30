56a1c18bf912c1bad1ceb7365bea6b7319543310
BrandonSalimTheHuman <114371928+BrandonSalimTheHuman@users.noreply.github.com>
2026-05-30 20:56:33 +1000

    Tried fixing woolworths one more time

--- woolworth_scraping-main/aldi_scraper.py
+++ woolworth_scraping-main/aldi_scraper.py
@@ -21,7 +21,7 @@ def scrape_aldi_specials(headless=False):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1080')  # maximize_window() is a no-op in headless
+        options.add_argument('--window-size=1920,1080') 
 
     options.add_argument('--log-level=3')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
@@ -95,7 +95,6 @@ def scrape_aldi_specials(headless=False):
                 retries = 3
                 while retries > 0:
                     try:
-                        # shadow_root = get_shadow_root(driver, host)
                         name = host.find_element(By.CSS_SELECTOR, '.product-tile__name').text.strip()
                         price = host.find_element(By.CSS_SELECTOR, '.base-price__regular').text.strip()
                                     
